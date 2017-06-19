68c37d3278f75ab15c6385b29635de06e6261858
lukas1421 <shijiluke@gmail.com>
2017-06-20 05:41:09 +0800

    config

--- config.R
+++ config.R
@@ -1,6 +1,7 @@
 
 #for desktop
-userName <- "LUke"
+
+userName <- Sys.getenv("RSTUDIO_USER_IDENTITY")
 tradingFolder <- paste0("C:\\Users\\",Sys.getenv("username"),"\\Desktop\\Trading\\")
 dataFolder <- "J:\\TDX\\T0002\\export\\"
 minuteDataFolder <- "J:\\TDX\\T0002\\export_1m\\"
@@ -10,8 +11,8 @@ cybDir <- "J:\\Data\\cybR\\"
 
 
 #for laptop
-#dataFolder <- "G:\\export\\"
-#minuteDataFolder <- "G:\\export_1m\\"
+dataFolder <- "G:\\export\\"
+minuteDataFolder <- "G:\\export_1m\\"
 cybDir <- "H:\\Data\\cybR\\"
 dayDataFolder <-  "G:\\export\\"
 
