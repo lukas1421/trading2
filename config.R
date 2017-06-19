b518f842414d0ae8421e3e7b289724caf3be9d1c
lukas1421 <shijiluke@gmail.com>
2017-06-19 11:32:25 +0800

    add config

---
+++ config.R
@@ -0,0 +1,20 @@
+
+#for desktop
+userName <- "LUke"
+tradingFolder <- paste0("C:\\Users\\",Sys.getenv("username"),"\\Desktop\\Trading\\")
+dataFolder <- "J:\\TDX\\T0002\\export\\"
+minuteDataFolder <- "J:\\TDX\\T0002\\export_1m\\"
+
+#cybDayDataFolder <- "J:\\Data\\cybR\\"
+cybDir <- "J:\\Data\\cybR\\"
+
+
+#for laptop
+#dataFolder <- "G:\\export\\"
+#minuteDataFolder <- "G:\\export_1m\\"
+cybDir <- "H:\\Data\\cybR\\"
+dayDataFolder <-  "G:\\export\\"
+
+
+
+
